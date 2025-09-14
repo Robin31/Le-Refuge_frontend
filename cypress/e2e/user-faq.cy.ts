f879be154d22ed5b9dccc06a543fcc749c6f2f80
Robin31 <robindenis@hotmail.fr>
2025-09-14 23:20:25 +0200

    Ajout du test e2e

--- cypress/e2e/user-faq.cy.ts
+++ cypress/e2e/user-faq.cy.ts
@@ -8,19 +8,15 @@ describe('Parcours utilisateur - Créer un compte et naviguer', () => {
   });
 
   it('devrait créer un compte, visiter FAQ puis page Chiens', () => {
-    // 1️⃣ Aller à la page d’inscription
     cy.visit('/signup');
 
-    // 2️⃣ Remplir le formulaire
     const email = `test${Date.now()}@test.com`;
     cy.get('#email').type(email);
     cy.get('#password').type('Test1234!');
     cy.get('#confirmPassword').type('Test1234!');
 
-    // 3️⃣ Cliquer sur le bouton "S'enregistrer"
     cy.contains("S'enregistrer").click();
 
-    // 4️⃣ Vérifier qu’on est redirigé vers la page d’accueil
     cy.wait('@register');
     cy.wait('@login');
 
@@ -28,23 +24,16 @@ describe('Parcours utilisateur - Créer un compte et naviguer', () => {
       expect(win.localStorage.getItem('token')).to.exist;
     });
 
-    // 5️⃣ Cliquer sur le bouton FAQ dans la navbar
     cy.get('i.pi-question').parent().click();
-    cy.url().then(url => {
-      console.log('URL après clic sur FAQ :', url);
-    });
 
-    // 6️⃣ Attendre l'appel d'API et afficher la page FAQ
     cy.wait('@getFaqs');
     cy.url().should('include', '/faq');
 
     cy.contains('En quoi abandonné son chien en foret est mal ?', { timeout: 10000 }).should('exist');
 
-    // 8️⃣ Aller à la page chiens
     cy.visit('/chiens');
     cy.url().should('include', '/chiens');
 
-    // 9️⃣ Vérifier un contenu de la page chiens
     cy.get('app-chien-card').first().click();
   });
 });
